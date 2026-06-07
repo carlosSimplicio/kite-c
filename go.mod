45cc119b40c393bd4ba27959de349fd2c27725bc
Carlos <carlos.edsmoura@gmail.com>
2026-06-07 00:12:42 -0300

    wip: understanding lipgloss and overlays

--- go.mod
+++ go.mod
@@ -5,9 +5,16 @@ go 1.26.4
 require charm.land/bubbletea/v2 v2.0.7
 
 require (
+	charm.land/bubbles/v2 v2.1.0 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
+)
+
+require (
+	charm.land/lipgloss/v2 v2.0.3
 	github.com/charmbracelet/colorprofile v0.4.3 // indirect
 	github.com/charmbracelet/ultraviolet v0.0.0-20260525132238-948f4557a654 // indirect
 	github.com/charmbracelet/x/ansi v0.11.7 // indirect
+	github.com/charmbracelet/x/exp/charmtone v0.0.0-20260602025833-85a30b5e440a
 	github.com/charmbracelet/x/term v0.2.2 // indirect
 	github.com/charmbracelet/x/termios v0.1.1 // indirect
 	github.com/charmbracelet/x/windows v0.2.2 // indirect
--- main.go
+++ main.go
@@ -3,32 +3,75 @@ package main
 import (
 	"io"
 	"log"
+	"os"
 	"strconv"
 	"strings"
 	"time"
 
 	"net/http"
 
+	"charm.land/bubbles/v2/textinput"
 	tea "charm.land/bubbletea/v2"
+	"charm.land/lipgloss/v2"
 )
 
-type model struct {
-	choices  []string
-	cursor   int
-	count    int
-	fs       bool
+type styles struct {
+	window  lipgloss.Style
+	overlay lipgloss.Style
+}
+
+type Model struct {
+	panels        []string
+	selectedPanel string
+	styles        *styles
+	width         int
+	height        int
+
+	searchActive bool
+	searchString string
+	textInput    textinput.Model
+
 	fetching bool
 	result   string
 }
 
-func initialModel() model {
-	return model{
-		choices: []string{"Search", "Add request", "Add query"},
-		fs:      false,
+func initialModel() Model {
+	s := new(styles)
+	s.window = lipgloss.NewStyle().Padding(2).Align(lipgloss.Center).Background(lipgloss.Color("1"))
+	s.overlay = lipgloss.NewStyle().Padding(2).Width(20).Height(5)
+	textInput := textinput.New()
+	textInput.Placeholder = "Busca"
+	textInput.Focus()
+	textInput.SetWidth(20)
+
+	return Model{
+		panels:        []string{"Rest Client", "SQL Client"},
+		selectedPanel: "Rest Client",
+		searchActive:  false,
+		styles:        s,
+		textInput:     textInput,
 	}
 }
 
-func (m model) Init() tea.Cmd {
+func (m *Model) setSelectedPanel(msg tea.KeyPressMsg) {
+	splitted := strings.Split(msg.String(), "")
+	panelNumberStr := splitted[len(splitted)-1]
+	panelNumber, err := strconv.Atoi(panelNumberStr)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	if len(m.panels) < panelNumber {
+		log.Println("panel number is greater than available panels")
+		return
+	}
+
+	m.selectedPanel = m.panels[panelNumber-1]
+	log.Printf("selectedPanel: %s\n", m.selectedPanel)
+}
+
+func (m Model) Init() tea.Cmd {
 	return nil
 }
 
@@ -50,53 +93,71 @@ func fetch() tea.Msg {
 	return fetchResult(rawBody)
 }
 
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	m.count++
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	log.Printf("message: %v", msg)
 	m.fetching = false
-	m.result = ""
+	m.searchString = ""
 
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		log.Printf("width: %d, height: %d", msg.Width, msg.Height)
+		m.styles.window = m.styles.window.Width(msg.Width)
+		m.styles.window = m.styles.window.Height(msg.Height)
+		m.width = msg.Width
+		m.height = msg.Height
+		return m, nil
+
 	case tea.KeyPressMsg:
 		switch msg.String() {
-		case "space":
-			m.cursor = 1
+		case "enter":
+			m.searchActive = false
+			m.searchString = m.textInput.Value()
+			m.textInput.Reset()
 			return m, nil
-		case "q":
+		case "ctrl+q":
 			return m, tea.Quit
-		case "f":
-			m.fs = !m.fs
-			return m, nil
 		case "g":
-			m.fetching = true
-			return m, fetch
-		default:
-			m.cursor = 2
+			if !m.searchActive {
+				m.fetching = true
+				return m, fetch
+			}
+		case "alt+1":
+			m.setSelectedPanel(msg)
+			log.Printf("model: %v", m)
 			return m, nil
+		case "alt+2":
+			m.setSelectedPanel(msg)
+			log.Printf("model: %v", m)
+			return m, nil
+		case "ctrl+k":
+			m.searchActive = true
+			return m, nil
+		case "esc":
+			m.searchActive = false
+			return m, nil
+		default:
+			log.Printf("%s was pressed", msg)
 		}
 	case fetchResult:
 		m.result = string(msg)
 	}
 
+	var cmd tea.Cmd
+	if m.searchActive {
+		m.textInput, cmd = m.textInput.Update(msg)
+		return m, cmd
+	}
+
 	return m, nil
 }
 
-func (m model) View() tea.View {
+func (m Model) View() tea.View {
 	var s strings.Builder
-	s.WriteString("Request-Query Lookup")
-	s.WriteString(": ")
-	s.WriteString(strconv.Itoa(m.count))
+	s.WriteString("Kite Client")
+	s.WriteString("\n")
 
-	for _, item := range m.choices {
-		s.WriteString("\n")
-		s.WriteString(item)
-	}
-
-	switch m.cursor {
-	case 1:
-		s.WriteString("\n\nspace was pressed")
-	case 2:
-		s.WriteString("\n\nDont know what happened dude")
-	}
+	s.WriteString(m.selectedPanel)
+	s.WriteString("\n")
 
 	if m.fetching {
 		s.WriteString("\n\nFetching your mom!")
@@ -107,16 +168,39 @@ func (m model) View() tea.View {
 		s.WriteString(m.result)
 	}
 
-	v := tea.NewView(s.String())
-
-	if m.fs {
-		v.AltScreen = true
+	if m.searchString != "" {
+		s.WriteString("\n\n\n")
+		s.WriteString("You just typed: ")
+		s.WriteString(m.searchString)
 	}
+
+	window := m.styles.window.Render(s.String())
+	windowLayer := lipgloss.NewLayer(window)
+
+	log.Printf("busca ativa: %t\n", m.searchActive)
+	if m.searchActive {
+		overlay := m.styles.overlay.Render(m.textInput.View())
+		overlayLayer := lipgloss.NewLayer(overlay).X(m.width / 2).Y(m.height / 2).Z(1)
+		windowLayer.AddLayers(overlayLayer)
+	}
+
+	compositor := lipgloss.NewCompositor(windowLayer)
+	v := tea.NewView(compositor.Render())
+	v.AltScreen = true
 
 	return v
 }
 
 func main() {
+	logOutputFile := "logs.txt"
+	file, err := os.OpenFile(logOutputFile, os.O_TRUNC|os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	log.SetOutput(file)
+
 	p := tea.NewProgram(initialModel())
 
 	if _, err := p.Run(); err != nil {
